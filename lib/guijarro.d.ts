7160b3098809a0eaa1f50d29db7fc73a6657bfc3
Emilio Platzer <emilioplatzer@gmail.com>
2018-04-15 23:49:51 -0300

    Agrego la botonera para centrar

--- lib/guijarro.d.ts
+++ lib/guijarro.d.ts
@@ -1,4 +1,6 @@
-declare function guijarro(targetDiv: string): {
+declare function guijarro(targetDiv: string, centerZone?: [number, number]): {
     addMark: (lat: number, long: number, abr: string, title: string, template?: any) => void;
     addLayer: (url: string, stlye?: any) => void;
+    map: ol.Map;
+    ol: typeof ol;
 };
